a572e9e8fc1d90523dfa672e7132e1587ceb345e
danieltmo <danieltmo3@gmail.com>
2019-12-08 15:25:29 -0300

    initial commit

---
+++ src/main/java/com/mycompany/ref_toy/A.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ref_toy;
+
+/**
+ *
+ * @author danie
+ */
+public class A {
+    
+    public void sur(){
+        source();
+    }
+    
+    public void source(){
+        int a =1;
+        a=2;
+        a=2;
+        
+        int b=1;
+        b=2;
+    }
+}
---
+++ src/main/java/com/mycompany/ref_toy/B.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ref_toy;
+
+/**
+ *
+ * @author danie
+ */
+public class B {
+    
+    A a = new A();
+
+    public void ext(){
+        a.source();
+    }
+    
+}
